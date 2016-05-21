15b70a19656d0ea575a2f3d43e13fa57d1738075
coolriver <670644959@qq.com>
2016-05-21 13:12:33 +0800

    update

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({ 
-    host: 'localhost', 
-    port: 8000 
+    host: '10.104.100.164', 
+    port: 80 
 });
 
 // Add the route
